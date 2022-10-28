826b715c7ddf39e86c372e4ce0594529baca065e
Jw95z <michaelmichael6909@gmail.com>
2022-10-28 13:57:31 -0700

    Changes to be committed: 	modified:   static/js/tetris.js

--- static/js/tetris.js
+++ static/js/tetris.js
@@ -324,8 +324,8 @@ function gameOver(){
     clearTimeout(movingThread);
     initExistField();
     if (!isQuit) {
-    alert("[Game Over 0000!!---!]\nLevel: "+level+"\nScore: "+score);
-    window.prompt("Enter your name")
+        alert("[Game Over 0000!!---!]\nLevel: "+level+"\nScore: "+score);
+        window.prompt("Enter your name")
     }
     document.getElementById("gameField").style.visibility = "hidden";
     document.getElementById("gameover").style.visibility = "visible";
