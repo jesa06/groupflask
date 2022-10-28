954826e3465606faf42eb6f171e52f64d29ee90a
Jw95z <michaelmichael6909@gmail.com>
2022-10-28 13:12:10 -0700

    Changes to be committed: 	modified:   static/js/tetris.js

--- static/js/tetris.js
+++ static/js/tetris.js
@@ -322,8 +322,10 @@ function changeShape(){
 // pause or end
 function gameOver(){
     clearTimeout(movingThread);
+    if (!isQuit) {
     alert("[Game Over 0000!!---!]\nLevel: "+level+"\nScore: "+score);
     window.prompt("Enter your name")
+    }
     initExistField();
     clearTimeout(movingThread);
     document.getElementById("gameField").style.visibility = "hidden";
