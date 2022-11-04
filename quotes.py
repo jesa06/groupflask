8d4846c30d5c0f5cabf61db730383381cec785d3
shruthim0 <shmalayath@gmail.com>
2022-11-04 13:25:02 -0700

    quotesapipy

---
+++ quotes.py
@@ -0,0 +1,22 @@
+import random
+
+quotes_data = []
+quotes_list = [
+    "Great job!",
+    "You got this!",
+    "Genius!!",
+    "YAY!!!",
+    "Perfect!",
+    "Amazing!",
+    "Score!!",
+    "WOOHOO!!",
+    "SLAY",
+    "You are amazing!",
+    "Woohoo!!",
+    "Keep it up!",
+    "Fantastic!",
+    "AMAZING!!",
+    "SO COOL!",
+    "YES!",
+    "WOAH!",
+]
