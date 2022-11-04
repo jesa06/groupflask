b511b88ebc3a2c0d3634009d3e7912c346bdba44
shruthim0 <shmalayath@gmail.com>
2022-11-04 13:55:02 -0700

    quotes.py

--- quotes.py
+++ quotes.py
@@ -1,7 +1,7 @@
 import random
 
 quotes_data = []
-quotes_list = [
+quotes_list = [   #making list of quotes
     "Great job!",
     "You got this!",
     "Genius!!",
@@ -19,4 +19,24 @@ quotes_list = [
     "SO COOL!",
     "YES!",
     "WOAH!",
+    "（っ＾▿＾）",
+    "ᕙ(`▿´)ᕗ",
 ]
+
+def initQuotes():
+    # setup dictionary with quotes
+    item_id = 0
+    for item in quotes_list:
+        quotes_list.append({"id": item_id, "quote": item})
+        item_id += 1
+
+def getQuote():
+    return(quotes_data)
+
+# Getting a quote
+def getQuote(id):
+    return(quotes_data[id])
+
+#Geting a random quote
+def getRandomJoke():
+    return(random.choice(quotes_data))
